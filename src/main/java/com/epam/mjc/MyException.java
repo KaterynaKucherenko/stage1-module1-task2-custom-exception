9596cdf6b9c69edfde5009c3525fdc11ded1cfb1
KaterynaKucherenko <estorskaya@gmail.com>
2023-01-05 18:30:23 +0300

    done

---
+++ src/main/java/com/epam/mjc/MyException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+
+public class MyException extends IllegalArgumentException{
+    public MyException(long id){
+        super ("Could not find student with ID" + id);
+    }
+}
